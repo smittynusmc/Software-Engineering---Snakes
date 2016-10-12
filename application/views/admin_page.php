d1d08216c2a841c9f04ac01f5ff8d38c1be5c0bd
Phong Tram <ptran@mmseducation.com>
2016-10-12 18:57:10 -0400

    Revert "add admin lte plugins, add admin page"
    This reverts commit 139ee311cf18f6cf829ce0437caa5dd2b3b03610.

--- application/views/admin_page.php
+++ application/views/admin_page.php
@@ -2,8 +2,6 @@
     <?php
     if (isset($this->session->userdata['logged_in'])) {
         $username = ($this->session->userdata['logged_in']['username']);
-        $user_firstname = ($this->session->userdata['logged_in']['user_firstname']);
-        $user_lastname = ($this->session->userdata['logged_in']['user_lastname']);
         $email = ($this->session->userdata['logged_in']['email']);
     } else {
         header("location: User_Authentication/login");
@@ -11,114 +9,25 @@
     ?>
     <head>
         <title>Admin Page</title>
-    <head>
-        <meta charset="utf-8">
-        <title>Login Form</title>
-        <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/AdminLTE.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+        <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
     </head>
-    <body class="hold-transition skin-blue sidebar-mini">
-        <aside class="main-sidebar">
-            <!-- sidebar: style can be found in sidebar.less -->
-            <section class="sidebar">
-                <!-- Sidebar user panel -->
-                <div class="user-panel">
-                    <p style="color:#fff"><?php echo $user_firstname. " " .$user_lastname; ?></p>
-                </div>
-             
-                <!-- sidebar menu: : style can be found in sidebar.less -->
-                <ul class="sidebar-menu">
-                    <li class="header">MAIN NAVIGATION</li>
-                    <li class="active treeview">
-                        <a href="#">
-                            <i class="fa fa-dashboard"></i> <span>Projects</span>
-                            <span class="pull-right-container">
-                                <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                        </a>
-                        <ul class="treeview-menu">
-                            <li><a href=""><i class="fa fa-circle-o"></i> View</a></li>
-                            <li><a href=""><i class="fa fa-circle-o"></i> Add</a></li>
-                        </ul>
-                        <a href="#">
-                            <i class="fa fa-dashboard"></i> <span>Bugs</span>
-                            <span class="pull-right-container">
-                                <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                        </a>
-                        <ul class="treeview-menu">
-                            <li><a href=""><i class="fa fa-circle-o"></i> View</a></li>
-                            <li><a href=""><i class="fa fa-circle-o"></i> Add</a></li>
-                        </ul>
-                    </li>
-                   
-                </ul>
-            </section>
-            <!-- /.sidebar -->
-        </aside>
-
-
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            
-
-            <!-- Main content -->
-            <section class="content">
-                <!-- Small boxes (Stat box) -->
-                
-
-            </section>
-            <!-- /.content -->
+    <body>
+        <div id="profile">
+            <?php
+            echo "Hello <b id='welcome'><i>" . $username . "</i> !</b>";
+            echo "<br/>";
+            echo "<br/>";
+            echo "Welcome to Admin Page";
+            echo "<br/>";
+            echo "<br/>";
+            echo "Your Username is " . $username;
+            echo "<br/>";
+            echo "Your Email is " . $email;
+            echo "<br/>";
+            ?>
+            <b id="logout"><a href="logout">Logout</a></b>
         </div>
-        <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <div class="pull-right hidden-xs">
-                <b>Version</b> 2.3.6
-            </div>
-            <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
-            reserved.
-        </footer>
-
-       
-    </div>
-    <!-- ./wrapper -->
-
-    <!-- jQuery 2.2.3 -->
-    <script src="<?php echo base_url(); ?>plugins/jQuery/jquery-2.2.3.min.js"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
-    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-    <script>
-        $.widget.bridge('uibutton', $.ui.button);
-    </script>
-    <!-- Bootstrap 3.3.6 -->
-    <script src="<?php echo base_url(); ?>bootstrap/js/bootstrap.min.js"></script>
-    <!-- Morris.js charts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-        <!-- Sparkline -->
-    <script src="<?php echo base_url(); ?>plugins/sparkline/jquery.sparkline.min.js"></script>
-    <!-- jvectormap -->
-    <script src="<?php echo base_url(); ?>plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-    <script src="<?php echo base_url(); ?>plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-    <!-- jQuery Knob Chart -->
-    <script src="<?php echo base_url(); ?>plugins/knob/jquery.knob.js"></script>
-    <!-- daterangepicker -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-    <script src="<?php echo base_url(); ?>plugins/daterangepicker/daterangepicker.js"></script>
-    <!-- datepicker -->
-    <script src="<?php echo base_url(); ?>plugins/datepicker/bootstrap-datepicker.js"></script>
-    <!-- Bootstrap WYSIHTML5 -->
-    <script src="<?php echo base_url(); ?>plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-    <!-- Slimscroll -->
-    <script src="<?php echo base_url(); ?>plugins/slimScroll/jquery.slimscroll.min.js"></script>
-    <!-- FastClick -->
-    <script src="<?php echo base_url(); ?>plugins/fastclick/fastclick.js"></script>
-    <!-- AdminLTE App -->
-    <script src="<?php echo base_url(); ?>dist/js/app.min.js"></script>
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="<?php echo base_url(); ?>dist/js/pages/dashboard.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo base_url(); ?>dist/js/demo.js"></script>
-</body>
+        <br/>
+    </body>
 </html>
