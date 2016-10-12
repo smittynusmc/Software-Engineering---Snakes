5724a23e9354ca42623a5d156ff366891e873922
Phong Tram <ptran@mmseducation.com>
2016-10-12 16:33:26 -0400

    fix page login

--- application/views/admin_page.php
+++ application/views/admin_page.php
@@ -4,7 +4,7 @@
         $username = ($this->session->userdata['logged_in']['username']);
         $email = ($this->session->userdata['logged_in']['email']);
     } else {
-        header("location: index.php");
+        header("location: User_Authentication/login");
     }
     ?>
     <head>
