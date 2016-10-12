84b3550f12e4a97edfee13f27e12ce5c4c52f9e0
Phong Tram <ptran@mmseducation.com>
2016-10-12 16:13:26 -0400

    format admin_page, fix css link

--- application/views/registration_form.php
+++ application/views/registration_form.php
@@ -9,9 +9,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     ?>
     <head>
         <title>Registration Form</title>
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
-        <link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.min.css">
     </head>
     <body>
         <div id="container" align="center" >
