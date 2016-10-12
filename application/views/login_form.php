6e51acd478983a5a216b340d09dfe7d8b5063ac1
Phong Tram <ptran@mmseducation.com>
2016-10-12 16:13:26 -0400

    format admin_page, fix css link

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -14,9 +14,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <head>
         <meta charset="utf-8">
         <title>Login Form</title>
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/dist/css/AdminLTE.min.css">
-        <link rel="stylesheet" href="/dist/css/skins/_all-skins.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/AdminLTE.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>dist/css/skins/_all-skins.min.css">
     </head>
    
     <body>
