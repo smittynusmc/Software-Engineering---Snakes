bae3fb46ef0bba349582acc3baa5101deab334f3
Phong Tram <ptran@mmseducation.com>
2016-10-27 11:43:44 -0400

    fix auto load

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database','encryption','helper');
+$autoload['libraries'] = array('database','encryption');
 
 /*
 | -------------------------------------------------------------------
