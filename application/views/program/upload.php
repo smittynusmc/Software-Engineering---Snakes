5b9a4fceb1a99e6ab2776ac79717f1bf70b1b83e
Cipolone <cipolonep0@elvix2.rowan.edu>
2016-11-17 16:27:03 -0500

    fixed upload file in program  Please enter the commit message for your changes. Lines starting

--- application/views/program/upload.php
+++ application/views/program/upload.php
@@ -8,7 +8,7 @@
             <h4 class="modal-title" id="project_insert-title"> Add New Project</h4>
         </div>
         <div class="modal-body" id="project_insert-body">
-            <?php echo form_open('Project/upload',array('id'=>'project_upload_form')); ?>
+            <?php echo form_open('Program/upload',array('id'=>'program_upload_form')); ?>
             <div class="box-body" >
                 <div class="form-error">
                     <?php echo validation_errors(); ?>
@@ -29,10 +29,10 @@
         </div>
         <div class="modal-footer" id="project_insert-footer">
             <div class="box-footer">
-                <button type="submit" class="btn btn-primary submit-button" formid="project_upload_form">Submit</button>
+                <button type="submit" class="btn btn-primary submit-button" formid="program_upload_form">Submit</button>
             </div>
         </div>
-        <div class="modal-footer" id="project_search-result">
+        <div class="modal-footer" id="program_search-result">
             <?php
             if(isset($result)){
                 if(isset($status) && $status=='success'){
