9e841b2fade2763e8dce658fa0e6d6cbf655170b
pt368 <ptran@mmseducation.com>
2016-12-15 21:04:28 -0500

    Delete log-2016-11-14.php

--- application/logs/log-2016-11-14.php
+++
@@ -1,7 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2016-11-20 08:38:02 --> Session: Unable to obtain lock for file '/var/lib/php/session/ci_session58fd997d7abe1ac83be0fee200b9f8719e13441b'.
-INFO - 2016-11-28 08:29:01 --> Hooks Class Initialized
-ERROR - 2016-11-28 08:29:01 --> Session: Unable to obtain lock for file '/var/lib/php/session/ci_session58fd997d7abe1ac83be0fee200b9f8719e13441b'.
-ERROR - 2016-11-28 08:29:01 --> Session: Unable to obtain lock for file '/var/lib/php/session/ci_session58fd997d7abe1ac83be0fee200b9f8719e13441b'.
-ERROR - 2016-11-28 08:29:01 --> Session: Unable to obtain lock for file '/var/lib/php/session/ci_session58fd997d7abe1ac83be0fee200b9f8719e13441b'.
